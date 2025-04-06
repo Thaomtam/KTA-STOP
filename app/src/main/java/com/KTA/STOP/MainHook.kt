3036f91caefdab7fd733e54103a928c8897517c2
Thảo mai tắm <109426941+Thaomtam@users.noreply.github.com>
2025-04-06 20:18:45 +0700

    Update MainHook.kt

--- app/src/main/java/com/KTA/STOP/MainHook.kt
+++ app/src/main/java/com/KTA/STOP/MainHook.kt
@@ -1,39 +1,29 @@
 package com.KTA.STOP
 
-import com.KTA.STOP.hook.BaseHook
-import com.KTA.STOP.hook.Launcher3Handler // Sửa tên class cho đồng bộ
+import android.util.Log
 import com.github.kyuubiran.ezxhelper.init.EzXHelperInit
-import com.github.kyuubiran.ezxhelper.utils.Log
-import com.github.kyuubiran.ezxhelper.utils.Log.logexIfThrow
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.IXposedHookZygoteInit
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
-private const val PACKAGE_NAME_HOOKED = "com.android.launcher3"
-private const val TAG = "KTA-STOP"
-
-class MainHook : IXposedHookLoadPackage, IXposedHookZygoteInit {
-    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        if (lpparam.packageName == PACKAGE_NAME_HOOKED) {
-            EzXHelperInit.initHandleLoadPackage(lpparam)
-            EzXHelperInit.setLogTag(TAG)
-            EzXHelperInit.setToastTag(TAG)
-            initHooks(Launcher3Handler()) // Khởi tạo instance của Launcher3Handler
-        }
+class Entry : IXposedHookLoadPackage, IXposedHookZygoteInit {
+    companion object {
+        lateinit var modulePath: String
     }
 
     override fun initZygote(startupParam: IXposedHookZygoteInit.StartupParam) {
+        modulePath = startupParam.modulePath
         EzXHelperInit.initZygote(startupParam)
     }
 
-    private fun initHooks(vararg hook: BaseHook) {
-        hook.forEach {
-            runCatching {
-                if (it.isInit) return@forEach
-                it.init()
-                it.isInit = true
-                Log.i("Inited hook: ${it.javaClass.simpleName}")
-            }.logexIfThrow("Failed init hook: ${it.javaClass.simpleName}")
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        EzXHelperInit.initHandleLoadPackage(lpparam)
+        
+        Log.d("KTASTOP", "handleLoadPackage: ${lpparam.packageName} ${lpparam.processName}")
+        val handler = when (lpparam.packageName) {
+            "com.android.launcher3" -> Launcher3Handler()
+            else -> return
         }
+        handler.handleLoadPackage(lpparam)
     }
 }
